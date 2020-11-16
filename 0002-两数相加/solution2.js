04addfb13d1e0f88ab9ef2343ead40eac42f9ba5
canwdev <c2097@live.com>
2020-11-16 18:35:45 +0800

    test

--- 0002-两数相加/solution2.js
+++ 0002-两数相加/solution2.js
@@ -10,75 +10,98 @@ function ListNode(val, next) {
 }
 
 // @lc code=start
-function ListNodeToArray(listNode) {
+function listNodeToArray(listNode) {
   var arr = []
 
   while (listNode.next) {
-    arr.push(listNode.val)
-    listNode = listNode.next
+      arr.push(listNode.val)
+      listNode = listNode.next
   }
   arr.push(listNode.val)
-  return arr.reverse()
+  return arr
 }
 function toListNode(arr) {
   var listNode, temp
   for (var i = 0; i < arr.length; i++) {
-    var newNode = new ListNode(arr[i])
-    if (!listNode) {
-      listNode = newNode
-    } else {
-      if (!temp) {
-        listNode.next = newNode
-        temp = listNode.next
+      var newNode = new ListNode(arr[i])
+      if (!listNode) {
+          listNode = newNode
       } else {
-        temp.next = newNode
-        temp = temp.next
+          if (!temp) {
+              listNode.next = newNode
+              temp = listNode.next
+          } else {
+              temp.next = newNode
+              temp = temp.next
+          }
       }
-    }
   }
   return listNode
 }
 function ArrayPlus(a1, a2) {
   var aShort, aLong
   if (a1.length > a2.length) {
-    aShort = a2
+      aShort = a2
+      aLong = a1
   } else {
-    aLong = a1
+      aShort = a1
+      aLong = a2
   }
 
-  var result = []
-  for (var i = 0; i < aLong.length; i++) {
-    var x = aShort[i]
-    var y = aLong[i]
-    var z = x ? x + y : y
-    result.push(z)
+  var result = [...aLong]
+  var carry = 0
+  for (var i = aShort.length - 1; i >= 0; i--) {
+      var x = aShort[i]
+      var y = aLong[i]
+      var sum = carry + x + y
+
+      if (sum >= 10) {
+          carry = 1
+          sum -= 10
+      } else {
+          carry = 0
+      }
+
+      result[i] = sum
   }
   return result
 }
 
 /**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
+* Definition for singly-linked list.
+* function ListNode(val, next) {
+*     this.val = (val===undefined ? 0 : val)
+*     this.next = (next===undefined ? null : next)
+* }
+*/
 /**
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
- */
-var addTwoNumbers = function (l1, l2) {
-  var a1 = ListNodeToArray(l1)
-  var a2 = ListNodeToArray(l2)
-
-  var arr = ArrayPlus(a1, a2).reverse()
-  console.log(arr)
-  return toListNode(arr)
+* @param {ListNode} l1
+* @param {ListNode} l2
+* @return {ListNode}
+*/
+var addTwoNumbers = function(l1, l2) {
+  var a1 = listNodeToArray(l1)
+  var a2 = listNodeToArray(l2)
+  console.log(a1, a2)
+  return toListNode(ArrayPlus(a1, a2).reverse())
 };
 // @lc code=end
 
-var l1 = new ListNode(2, new ListNode(4, new ListNode(3)))
-var l2 = new ListNode(5, new ListNode(6, new ListNode(4)))
+var l1 = toListNode([2, 4, 3])
+var l2 = toListNode([5, 6, 4])
+
+/*
+[2, 4, 3]
+[5, 6, 4]
+
+=> [7,0,8]
+*/
 
 console.log(addTwoNumbers(l1, l2))
+
+/*
+[9,9,9,9,9,9,9]
+[9,9,9,9]
+
+=> [8,9,9,9,0,0,0,1]
+*/
