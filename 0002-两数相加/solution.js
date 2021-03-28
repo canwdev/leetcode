b44efcde2b1199d493062d156f004b53ccd43379
canwdev <c2097@live.com>
2021-03-28 11:13:14 +0800

    update

--- 0002-两数相加/solution.js
+++ 0002-两数相加/solution.js
@@ -10,23 +10,14 @@ function ListNode(val, next) {
 }
 
 // 数组转换为链表
-function toListNode(arr) {
-  var listNode, cur
-  for (var i = 0; i < arr.length; i++) {
-      var newNode = new ListNode(arr[i])
-      if (!listNode) {
-          listNode = newNode
-      } else {
-          if (!cur) {
-              listNode.next = newNode
-              cur = listNode.next
-          } else {
-              cur.next = newNode
-              cur = cur.next
-          }
-      }
+const arrToListNode = (arr) => {
+  let pre = new ListNode(arr[0])
+  let cur = pre
+  for (let i = 1; i < arr.length; i++) {
+    cur.next = new ListNode(arr[i])
+    cur = cur.next
   }
-  return listNode
+  return pre
 }
 
 // @lc code=start
@@ -42,59 +33,64 @@ function toListNode(arr) {
 * @param {ListNode} l2
 * @return {ListNode}
 */
-var addTwoNumbers = function(l1, l2) {
+var addTwoNumbers = function (l1, l2) {
   if (!l1)
-      return l2
+    return l2
   if (!l2)
-      return l1
+    return l1
 
   var pre = new ListNode(0) // 创建结果链表
   var cur = pre
 
   var carry = 0 // 进位
   while (l1 !== null && l2 !== null) {
-      var sum = l1.val + l2.val + carry
-      var res = sum % 10 // 使用取余运算，获得个位数
-      carry = parseInt(sum / 10) // 获得进位
+    var sum = l1.val + l2.val + carry
+    var dig = sum % 10 // 使用取余运算，获得个位数
+    carry = parseInt(sum / 10) // 获得进位
+    // console.log('carry=', carry)
 
-      cur.next = new ListNode(res)
-      cur = cur.next
+    cur.next = new ListNode(dig)
+    cur = cur.next
 
-      l1 = l1.next
-      l2 = l2.next
+    l1 = l1.next
+    l2 = l2.next
   }
 
   // 遍历剩下的链表项（如果有）
   function lastList(l) {
-      while (l) {
-          var sum = l.val + carry
-          var res = sum % 10
-          carry = parseInt(sum / 10)
-          cur.next = new ListNode(res)
-          cur = cur.next
+    while (l) {
+      var sum = l.val + carry
+      var dig = sum % 10
+      carry = parseInt(sum / 10)
+      // console.log('l carry=', carry)
 
-          l = l.next
-      }
+      cur.next = new ListNode(dig)
+      cur = cur.next
+
+      l = l.next
+    }
   }
   if (l1 !== null) {
-      lastList(l1)
+    lastList(l1)
   }
   if (l2 !== null) {
-      lastList(l2)
+    lastList(l2)
   }
 
   // 最后的进位项
   if (carry !== 0) {
-      cur.next = new ListNode(carry)
-      cur = cur.next
+    // console.log('f carry=', carry)
+
+    cur.next = new ListNode(carry)
+    cur = cur.next
   }
 
   return pre.next
 };
 // @lc code=end
 //                   8 9 9 9 0 0 0 1
-var l1 = toListNode([9,9,9,9,9,9,9])
-var l2 = toListNode([9,9,9,9])
+var l1 = arrToListNode([9, 9, 9, 9, 9, 9, 9])
+var l2 = arrToListNode([9, 9, 9, 9])
 
 console.log(addTwoNumbers(l1, l2))
 
