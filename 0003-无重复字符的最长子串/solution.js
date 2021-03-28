cfdf0353d9449cbb412354a12866c933d2589a7e
canwdev <c2097@live.com>
2021-03-28 12:15:42 +0800

    solution

--- 0003-无重复字符的最长子串/solution.js
+++ 0003-无重复字符的最长子串/solution.js
@@ -9,8 +9,33 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
+var lengthOfLongestSubstring = function (s) {
+  if (!s.length) {
+    return 0
+  }
 
+  const hashSet = new Set()
+  let max = 1
+
+  let start = 0, end = 0
+
+  while (end < s.length) {
+    const cEnd = s.charAt(end)
+
+    if (hashSet.has(cEnd)) {
+      hashSet.delete(s.charAt(start))
+      start++
+    } else {
+      hashSet.add(cEnd)
+      max = Math.max(max, hashSet.size)
+      end++
+    }
+  }
+  console.log('hashSet', hashSet)
+  return max
 };
 // @lc code=end
 
+const result = lengthOfLongestSubstring('abcabcbb')
+
+console.log('result', result)
